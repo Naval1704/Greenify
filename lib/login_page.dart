c757ddd684a7180bf298b40ec9c56d7d9ec7c1b5
Gaurav Naval <naval.2@iitj.ac.in>
2023-08-29 01:06:29 +0530

    Code changes Gradient Colors

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,6 +9,13 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(title: Text('Login')),
+      // decoration: const BoxDecoration(
+      //   gradient: LinearGradient(
+      //     colors: [Color(0xFF1EFF34), Color(0xFF14FF00)],
+      //     begin: Alignment.bottomLeft,
+      //     end: Alignment.topRight,
+      //   ),
+      // ),
       backgroundColor: Colors.green,
       body: Center(
         child: Column(
@@ -36,26 +43,45 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
                 // ignore: deprecated_member_use
                 primary: Colors.red, // Set the button color to green
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16), // Increase button size
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 40, vertical: 16), // Increase button size
               ),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const FarmerPage()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FarmerPage()));
               },
-              child: const Text('Farmer', style: TextStyle(fontSize: 18,color: Colors.black, fontFamily: 'Aclonica' ), ),
+              child: const Text(
+                'Farmer',
+                style: TextStyle(
+                    fontSize: 18, color: Colors.black, fontFamily: 'Aclonica'),
+              ),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)), backgroundColor: Colors.red, // Set the button color to green
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16), // Increase button size
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                backgroundColor: Colors.red, // Set the button color to green
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 40, vertical: 16), // Increase button size
               ),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const FamilyPage()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FamilyPage()));
               },
-              child: const Text('Family', style: TextStyle(fontSize: 18,color: Colors.black, fontFamily: 'Aclonica')),
+              child: const Text('Family',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontFamily: 'Aclonica')),
             ),
           ],
         ),
