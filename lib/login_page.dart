4c05f586bdb35f65b603db6710a231ff6f0f14da
Gaurav Naval <naval.2@iitj.ac.in>
2023-08-29 01:41:23 +0530

    Login_page.dart file Changed : Font colors, Background color, Height diff, font size

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,47 +9,51 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(title: Text('Login')),
-      // decoration: const BoxDecoration(
-      //   gradient: LinearGradient(
-      //     colors: [Color(0xFF1EFF34), Color(0xFF14FF00)],
-      //     begin: Alignment.bottomLeft,
-      //     end: Alignment.topRight,
-      //   ),
-      // ),
       backgroundColor: Colors.green,
-      body: Center(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF1EFF34), Color(0xFF47FF4B), Color(0xFF14FF00)],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('assets/agriculture.png'),
+            const SizedBox(height: 40),
             const Text(
               'Greenify',
               style: TextStyle(
                 fontFamily: 'Aclonica', // Use the 'Acrolinca' font
                 fontSize: 36,
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.black,
               ),
             ),
             const SizedBox(height: 20),
             const Text(
-              'Who are you?',
+              'Who are you ?',
               style: TextStyle(
                 fontFamily: 'Aclonica',
-                color: Colors.white,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
                 fontSize: 18,
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                // ignore: deprecated_member_use
-                primary: Colors.red, // Set the button color to green
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 40, vertical: 16), // Increase button size
-              ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  // ignore: deprecated_member_use
+                  primary:
+                      const Color(0xFFFF3737), // Set the button color to green
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                  shadowColor: Colors.black // Increase button size
+                  ),
               onPressed: () {
                 Navigator.push(
                     context,
@@ -59,28 +63,33 @@ class LoginPage extends StatelessWidget {
               child: const Text(
                 'Farmer',
                 style: TextStyle(
-                    fontSize: 18, color: Colors.black, fontFamily: 'Aclonica'),
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontFamily: 'Aclonica',
+                ),
               ),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                backgroundColor: Colors.red, // Set the button color to green
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 40, vertical: 16), // Increase button size
-              ),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  backgroundColor:
+                      const Color(0xFFFF3737), // Set the button color to green
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                  shadowColor: Colors.black // Increase button size
+                  ),
               onPressed: () {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
                         builder: (context) => const FamilyPage()));
               },
-              child: const Text('Family',
+              child: const Text(' Family ',
                   style: TextStyle(
                       fontSize: 18,
-                      color: Colors.black,
+                      color: Colors.white,
                       fontFamily: 'Aclonica')),
             ),
           ],
