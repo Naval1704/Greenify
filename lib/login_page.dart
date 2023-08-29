fa3d8af41431511d943d4fea6eddb876d2ee2c7e
Gaurav Naval <naval.2@iitj.ac.in>
2023-08-29 18:26:08 +0530

    Changed UI of Start page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,92 +12,119 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.green,
       body: Stack(
         children: [
-          Positioned(
-            right: -500,
-            bottom: -400,
-            child: Opacity(
-              opacity: 0.8, // Adjust opacity as needed
-              child: Image.asset(
-                'assets/watermark.png', // Replace with your watermark image path
-                width: 950, // Adjust width as needed
+          // Positioned(
+          //   right: -500,
+          //   bottom: -400,
+          //   child: Opacity(
+          //     opacity: 0.9, // Adjust opacity as needed
+          //     child: Image.asset(
+          //       'assets/watermark.png', // Replace with your watermark image path
+          //       width: 950, // Adjust width as needed
+          //     ),
+          //   ),
+          // ),
+          Container(
+            decoration: const BoxDecoration(
+              // Gradient Background
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF1EFF34),
+                  Color(0xFF47FF4B),
+                  Color(0xFF14FF00)
+                ],
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
               ),
             ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Greenify',
-                  style: TextStyle(
-                    fontFamily: 'Aclonica', // Use the 'Aclonica' font
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Who are you?',
-                  style: TextStyle(
-                    fontFamily: 'Aclonica',
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    primary: Colors.red, // Set the button color to red
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 16),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const FarmerPage()),
-                    );
-                  },
-                  child: const Text(
-                    'Farmer',
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Greenify',
                     style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontFamily: 'Aclonica'),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+                      fontFamily: 'Aclonica', // Use the 'Aclonica' font
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    primary: Colors.red, // Set the button color to red
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 16),
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const FamilyPage()),
-                    );
-                  },
-                  child: const Text(
-                    'Family',
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Who are you?',
                     style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontFamily: 'Aclonica'),
+                      fontFamily: 'Aclonica',
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        primary: Colors.red, // Set the button color to red
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 40, vertical: 16),
+                        shadowColor: Colors.black),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FarmerPage()),
+                      );
+                    },
+                    child: const Text(
+                      'Farmer',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontFamily: 'Aclonica'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      primary: Colors.red, // Set the button color to red
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 16),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const FamilyPage()),
+                      );
+                    },
+                    child: const Text(
+                      ' Family ',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontFamily: 'Aclonica'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Positioned(
+                    right: -500,
+                    bottom: -400,
+                    child: Opacity(
+                      opacity: 0.9, // Adjust opacity as needed
+                      child: Image.asset(
+                        'assets/watermark.png', // Replace with your watermark image path
+                        width: 950, // Adjust width as needed
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
+          )
         ],
       ),
     );
