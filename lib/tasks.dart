bdeecf37686c4a8f830167a334f411b33894a37c
Gaurav Naval <naval.2@iitj.ac.in>
2023-10-28 15:57:09 +0530

    make changes in Task.dart also

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Tasks extends StatefulWidget {
   const Tasks({Key? key}) : super(key: key);
@@ -9,129 +8,143 @@ class Tasks extends StatefulWidget {
 }
 
 class _TasksState extends State<Tasks> {
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         top: true,
-        child: Stack(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
           children: [
-            Container(
-              width: double.infinity,
-              height: 50,
-              decoration: const BoxDecoration(
-                color: Colors.white,
+            const Text(
+              'Pending Tasks',
+              style: TextStyle(
+                fontFamily: 'Sans',
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            const Positioned(
-              top: 15,
-              left: 20,
-              child: Text(
-                'Pending Tasks',
-                style: TextStyle(
-                  fontFamily: 'Aclonica',
-                  fontSize: 16,
-                ),
-              ),
+            TaskTile(
+              imagePath: 'assets/Mango.png',
+              taskName: 'Task 1 Name',
+              taskDate: 'Task 1 Date',
+              taskType: 'Task 1 Type',
+              taskDescription: 'This is the description for Task 1.',
             ),
-            Align(
-              alignment: const AlignmentDirectional(0.00, 1.00),
-              child: Padding(
-                padding: EdgeInsets.only(top: 100),
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 25),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(mainAxisSize: MainAxisSize.max, children: [
-                        Container(
-                          width: 200,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF6CFF69),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(20),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(22),
-                              child: Image.network(
-                                'https://picsum.photos/seed/559/600',
-                                width: double.infinity,
-                                height: double.infinity,
-                                fit: BoxFit.contain,
-                                alignment: Alignment(0.00, 0.00),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 200,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF6CFF69),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(20),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: Image.network(
-                                'https://picsum.photos/seed/559/600',
-                                width: 300,
-                                height: 200,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: 200,
-                          height: 200,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF6CFF69),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.all(20),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(25),
-                              child: Image.network(
-                                'https://picsum.photos/seed/236/600',
-                                width: 300,
-                                height: 200,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ]),
-                    ),
-                  ),
-                ),
-              ),
+            TaskTile(
+              imagePath: 'path_to_image2.png',
+              taskName: 'Task 2 Name',
+              taskDate: 'Task 2 Date',
+              taskType: 'Task 2 Type',
+              taskDescription: 'This is the description for Task 2.',
             ),
+            // Add more TaskTiles for additional tasks
           ],
         ),
       ),
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  final String imagePath;
+  final String taskName;
+  final String taskDate;
+  final String taskType;
+  final String taskDescription;
+
+  TaskTile({
+    required this.imagePath,
+    required this.taskName,
+    required this.taskDate,
+    required this.taskType,
+    required this.taskDescription,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _showTaskDetails(context);
+      },
+      child: Padding(
+        padding:
+            const EdgeInsets.only(bottom: 20.0), // Add spacing between boxes
+        child: Card(
+          elevation: 3, // Add elevation for a card-like effect
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                imagePath,
+                height: 150, // Adjust the height as needed
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      taskName,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Date: $taskDate\nType: $taskType',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showTaskDetails(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                imagePath,
+                height: 150, // Adjust the height as needed
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              Text('Date: $taskDate'),
+              Text('Type: $taskType'),
+              Text('Description: $taskDescription'),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
